6d6c2363ec57f2e9fd56e23881e9ac9a3f58b4cb
CarlosW1998 <carlos-walte@hotmail.com>
2021-11-22 09:23:54 -0300

    Env

--- app.py
+++ app.py
@@ -3,6 +3,8 @@ from flask import Flask
 
 app = Flask(__name__)
 
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///example.sqlite"
+
 if __name__ == '__main__':
     from routes.index import *
     app.run()
